bd8eae2b029e2f837682c29a095f02972564b30a
Eron Moraes <110741158+eron-moraes@users.noreply.github.com>
2025-02-19 03:57:01 -0300

    exercicios 02

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,7 +1,19 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
-  it('verifica o título da aplicação', () => {
+  beforeEach(() => {
     cy.visit('./src/index.html')
+  })
 
+  it('verifica o título da aplicação', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName').type('Eron')
+    cy.get('#lastName').type('Moraes')
+    cy.get('#email').type('eron.moraes7@gmail.com')
+    cy.get('#open-text-area').type('Obrigado!')
+    cy.get('button[type="submit"]').click()
+    
+    //Mensagem enviada com sucesso.
+    cy.get('.success').should('be.visible')
   })
 })
