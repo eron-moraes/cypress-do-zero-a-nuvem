1dbf59e3069e5f10d8636c77e06ebfe2eb9ae16c
Eron Moraes <110741158+eron-moraes@users.noreply.github.com>
2025-02-26 23:03:01 -0300

    ex5

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -12,8 +12,69 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#email').type('eron.moraes7@gmail.com')
     cy.get('#open-text-area').type('Obrigado!')
     cy.get('button[type="submit"]').click()
-    
+
     //Mensagem enviada com sucesso.
     cy.get('.success').should('be.visible')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('Eron')
+    cy.get('#lastName').type('Moraes')
+    cy.get('#email').type('eron.moraes7@gmail,com')
+    cy.get('#open-text-area').type('Obrigado!')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
+  it('campo telefone continua vazio quando preenchido com valor não-numerico', () => {
+    cy.get('#phone')
+      .type('abcde')
+      .should('have.value', '')
+  })
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('Eron')
+    cy.get('#lastName').type('Moraes')
+    cy.get('#email').type('eron.moraes7@gmail.com')
+    cy.get('#open-text-area').type('Obrigado!')
+    cy.get('#phone-checkbox').click()
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName')
+      .type('Eron')
+      .should('have.value', 'Eron')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#lastName')
+      .type('Moraes')
+      .should('have.value', 'Moraes')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#email')
+      .type('eron.moraes7@gmail.com')
+      .should('have.value', 'eron.moraes7@gmail.com')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#open-text-area')
+      .type('Obrigado!')
+      .should('have.value', 'Obrigado!')
+      .clear()
+      .should('have.value', '')
+
+      cy.get('#phone')
+      .type('1290000000')
+      .should('have.value', '1290000000')
+      .clear()
+      .should('have.value', '')
+  })
+  it.only('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios.', () => {
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
   })
 })
