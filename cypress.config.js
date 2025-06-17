39e295577f51dd9b5a47fa448853433c5349c0af
Eron Moraes <eron.moraes7@gmail.com>
2025-06-17 06:27:26 -0300

    Configure the ci workflow

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,17 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {},
+  setupNodeEvents(on, config) {
+    require('cypress-mochawesome-reporter/plugin')(on);
+  },
 })
 
